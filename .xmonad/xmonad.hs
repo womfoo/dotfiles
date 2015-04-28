4f888fcfeb742c392d656241b1ae52d795ba91c7
Kranium Gikos Mendoza <kranium@gikos.net>
2015-04-28 18:45:59 +0000

    add brightness keys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -32,6 +32,8 @@ main = do
     `additionalKeys` [
                       ((mod4Mask, xK_b), sendMessage ToggleStruts)
                      ,((mod4Mask, xK_l), unsafeSpawn "slimlock")
+                     ,((0,xF86XK_MonBrightnessUp), spawn "light -A 10")
+                     ,((0,xF86XK_MonBrightnessDown), spawn "light -U 10")
                      ,((0,xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume 0 +1.5%")
                      ,((0,xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume 0 -- -1.5%")
                      ,((0,xF86XK_AudioMute), spawn "pactl set-sink-mute 0 toggle")
