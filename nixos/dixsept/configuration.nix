e354240497a0398ef37d6641150f7e23a163dab2
Kranium Gikos Mendoza <kranium@gikos.net>
2016-01-15 12:36:49 +0000

    enable virtualbox

--- nixos/dixsept/configuration.nix
+++ nixos/dixsept/configuration.nix
@@ -80,7 +80,7 @@
     { createHome      = true;
       home            = "/home/kranium";
       description     = "your name";
-      extraGroups     = [ "wheel" ];
+      extraGroups     = [ "vboxusers" "wheel" ];
       useDefaultShell = true;
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCv8Rdf8gqewljlONxIU/NoI+aQhA0UNQbAsif0gKqGLPG2QrZgPktgG3r0Fn6cKtuhy7iExfWmUafJU73Od/hj8DK6uxicHEXh5pv6DZc2DEwyC5orJHQOZLblo96u2xsBkVx/++Nq/2vW1aMN0Wg8/Vgal1fBcfJAT9XAFmiKXLZxIvxWWw0PZYil4QJtlVGwebXm1trPr7H9hV8l+Lse8Z/Xt38DzQJI7yV5m6ENxPL/xCFsMMgb27c+Xf6gJPq2DIcUOJiP7fOcHXWN2W4/+ApUH5adMhJ8Y8mT4CGcLqNhcHKSFzPaUQpfQ0vi3QJez1LYoHANu6Iy6q7HoIab kranium@silverspark"
@@ -99,5 +99,7 @@
   services.syncthing.enable = true;
   services.syncthing.user = "kranium";
   services.syncthing.dataDir = "/data/syncthing";
+
+  virtualisation.virtualbox.host.enable = true;
 
 }
