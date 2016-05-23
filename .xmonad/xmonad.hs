7e06c86549ddfcb1974204a49205393ead421d94
Kranium Gikos Mendoza <kranium@gikos.net>
2016-05-23 15:48:25 +0000

    fix volume control parameters

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -39,7 +39,8 @@ main = do
                      ,((mod4Mask, xK_l), unsafeSpawn "slimlock")
                      ,((0,xF86XK_MonBrightnessUp), spawn "light -A 10")
                      ,((0,xF86XK_MonBrightnessDown), spawn "light -U 10")
-                     ,((0,xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume 0 +1.5%")
-                     ,((0,xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume 0 -- -1.5%")
-                     ,((0,xF86XK_AudioMute), spawn "pactl set-sink-mute 0 toggle")
+                     ,((0,xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo +1.5%")
+                     ,((0,xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo -1.5%")
+                     ,((0,xF86XK_AudioMute), spawn "pactl set-sink-mute alsa_output.pci-0000_00_1b.0.analog-stereo toggle")
                      ]
+-- instead of 0 use alsa_output.pci-0000_00_1b.0.analog-stereo
