da434126000f106e4c8362cd71f6ba62dd9ea794
Kranium Gikos Mendoza <kranium@gikos.net>
2019-01-06 07:40:11 +0000

    update puppet sandbox

--- envs/puppet/shell.nix
+++ envs/puppet/shell.nix
@@ -2,14 +2,18 @@ with import <nixpkgs> {}; {
   rubyEnv = stdenv.mkDerivation {
     name = "puppet-env";
     buildInputs = [
-      ruby_2_1
+      ruby_2_4
       facter
+      augeas
     ];
     shellHook = ''
       export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
       export RUBYLIB=${facter}/lib/ruby
-      export GEM_PATH=/home/kranium/.gem/ruby/2.1.0
-      export PATH=$PATH:~/.gem/ruby/2.1.0/bin
+      export GEM_PATH=/home/kranium/.gem/ruby/2.4.0
+      export PATH=$PATH:~/.gem/ruby/2.4.0/bin
     '';
   };
 }
+
+# gem install puppet --user
+# gem install ruby-augeas --user
