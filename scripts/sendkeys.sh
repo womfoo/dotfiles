e6034638bc52e1ca1eebacd26d79fdd8ebd8caac
Kranium Gikos Mendoza <kranium@gikos.net>
2019-03-31 10:47:27 +0000

    sendkeys script for quicker typing

---
+++ scripts/sendkeys.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+KEY_DIR=/home/kranium/encfs/darcs.private/gikos-it/keys
+YAML_FILE=~/sendkeys.yaml
+
+case $1 in
+  password)
+      ENTRY=$(yq -r '.passwords | keys[]' $YAML_FILE | dmenu)
+      SECRET=$(eyaml decrypt --pkcs7-private-key=$KEY_DIR/private_key.pkcs7.pem --pkcs7-public-key=$KEY_DIR/public_key.pkcs7.pem -e $YAML_FILE | yq -r .passwords.$ENTRY | sed -e 's/^DEC::PKCS7\[//' -e 's/]!$//')
+  ;;
+  totp)
+      ENTRY=$(yq -r '.totps | keys[]' $YAML_FILE | dmenu)
+      TOKEN=$(eyaml decrypt --pkcs7-private-key=$KEY_DIR/private_key.pkcs7.pem --pkcs7-public-key=$KEY_DIR/public_key.pkcs7.pem -e $YAML_FILE | yq -r .totps.$ENTRY | sed -e 's/^DEC::PKCS7\[//' -e 's/]!$//')
+      SECRET=$(oathtool --totp -b $TOKEN)
+  ;;
+  alias)
+      SECRET=$(yq -r .aliases[] | sed -e 's/^DEC::PKCS7\[//' -e 's/]!$//')
+  ;;
+  *)
+  echo "invalid usage"
+  exit 1
+  ;;
+esac
+
+xdotool type "$SECRET"
