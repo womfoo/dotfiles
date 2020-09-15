9ccf3c8905a2c80b7620d3e117a26212d65250aa
Kranium Gikos Mendoza <kranium@gikos.net>
2020-09-15 14:38:34 +0000

    silverspark: office printer

--- nixos/silverspark/configuration.nix
+++ nixos/silverspark/configuration.nix
@@ -677,6 +677,7 @@ in
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.fxlinuxprint ];
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
